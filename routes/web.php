cac96185a076221b343add663978aabec3401768
sadiaNawshin <sadia.nawshin@g.bracu.ac.bd>
2024-07-06 11:32:20 +0600

    profile_update

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\profile_controller;
-Route::get('/', function () {
-    return view('index');
-});
-Route::get("/profile_update",[profile_controller::class, "index"])->name('sadia');
-Route::post("/register",[profile_controller::class, "register"]);
-=======
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Auth;
 
+Route::get("/profile_update",[profile_controller::class, "index"])->name('sadia');
+Route::post("/form_update",[profile_controller::class, "register"]);
 Auth::routes();
 Route::get('/', function () {
     return view('index');
 });
 
 
-Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\DashboardController::class, 'index'])->name('home');
 Route::get('/welcome', [App\Http\Controllers\HomeController::class, 'welcome'])->name('welcome');
 
+// Route::get('/getData',function(){
+
+//     echo 'Abir';
+
+
+// })->name('Abir');
 
 Route::get('/signup', function(){
     return view('auth.register');
 })->name('sign-up');
-
->>>>>>> 9651c7bcee75d0b6e6b2aac860ed60c92d1c17c1
