981e80a35ae340a8f7d88812896712abee2b34fe
sadiaNawshin <sadia.nawshin@g.bracu.ac.bd>
2024-08-07 13:20:06 +0600

    table created

--- resources/views/donation/donation_form.blade.php
+++ resources/views/donation/donation_form.blade.php
@@ -1,40 +1,6 @@
 @extends('layouts.app')
 @section('content')
 
-<!DOCTYPE html>
-<html lang="zxx">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Orphan - Donation Page</title>
-  <link rel="icon" href="assets/img/logo-icon.png">
-
-   <!-- CSS only -->
-   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-   <!-- Font Awesome 6 -->
-   <link rel="stylesheet" href="assets/css/fontawesome.min.css">
-   <link rel="stylesheet" href="assets/css/nice-select.css">
-   <!-- style -->
-   <link rel="stylesheet" href="assets/css/style.css">
-   <link rel="stylesheet" href="assets/css/style-dark.css">
-   <!-- responsive -->
-   <link rel="stylesheet" href="assets/css/responsive.css">
-   <!-- color -->
-   <link rel="stylesheet" href="assets/css/color.css">
- </head>
-<body>
-  <div class="preloader">
-    <div class="sec-loading">
-      <div class="one">
-      </div>
-    </div>
-  </div>
-<!-- theme color -->
-    <div class="theme-color">
-      <img src="assets/img/sun.png" alt="moon" id="theme-icon">
-    </div>
-  <!-- theme color end -->
-
 <section class="page-title-area" style="background-image:url(https://via.placeholder.com/1920x430)">
   <div class="container">
     <div class="title-area-data">
@@ -71,6 +37,7 @@
           <h5>Enter your billing information</h5>
           <label>Email Address</label>
           <input type="text" name="Email">
+          
           <label>Full Name</label>
           <input type="text" name="name">
           <label>City</label>
@@ -86,7 +53,7 @@
             </select>
             <input type="text" name="zidcode" placeholder="Zipcode">
           </div>
-          <input type="submit" class="give-submit" name="give-purchase" value="Donate Now">
+          <input type="submit" class="give-submit" name="give-purchase" value="Save">
         </div>
         <div class="enter-your-information-data payment">
           <h5>Enter Your Payment Information</h5>
@@ -132,15 +99,5 @@
   </div>
 </section>
 
-<!-- footer end -->
-<!-- Back to top button -->
-  <a id="button"></a>
-<!-- Back to top button end -->
-<!-- jQuery -->
-<script src="assets/js/jquery-3.6.0.min.js"></script>
-<!-- Bootstrap Js -->
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/jquery.nice-select.min.js"></script>
-<script src="assets/js/custom.js"></script>
-</body>
+
 @endsection
